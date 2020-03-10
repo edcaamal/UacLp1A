e4351ef14bb2f01f43892c2a00fa2a1eea81fa65
edgar <edgar@localhost>
2020-03-09 22:21:52 -0600

    Primer Proyecto Git

---
+++ GitHub1a/src/github1a/GitHub1a.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github1a;
+
+/**
+ *
+ * @author edgar
+ */
+public class GitHub1a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        System.out.println("Saludos UAC-FI-ISC 2020");
+        
+    }
+    
+}
